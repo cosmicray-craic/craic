207d559d5fa2ad4595b48d27649df86ce99c4bea
nghayden003 <nghayden--3ucl01@gmail.com>
2025-07-24 11:54:59 +0200

    Fixed gamma-ray energy range, add Eg_lo and Eg_hi as free parameters

--- craic/SNR_Cloud_Flux.py
+++ craic/SNR_Cloud_Flux.py
@@ -11,32 +11,7 @@ tran = transport()
 acce = accelerator()
 fl = flux()
 
-# Set up initial energy arrays
-N = 200 
-Eg_lo = 10. *u.GeV
-Eg_hi = 3e3 *u.TeV # 1 PeV
 
-# Set up gamma-ray evaluation energies
-Eg_edges = np.logspace(start = np.log10(Eg_lo.to(u.TeV).value),
-                    stop  = np.log10(Eg_hi.to(u.TeV).value),
-                    num   = N+1) *u.TeV
-Egs  = np.sqrt(Eg_edges[:-1] * Eg_edges[1:]) # log bin centers
-dEgs = np.diff(Eg_edges)
-
-# Define proton energies
-Ep_edges = np.logspace(0, 6.48, 1001) *u.GeV #1 PeV
-Eps = np.sqrt(Ep_edges[:-1] * Ep_edges[1:]) # log bin centers
-dEps = np.diff(Ep_edges)
-
-#Define low energy part of proton energies 
-Epl_edges = np.logspace(0, 3.24, 1001) *u.GeV
-Epls = np.sqrt(Epl_edges[:-1] * Epl_edges[1:]) 
-dEpls = np.diff(Epl_edges)
-
-m_pion = 135. * u.MeV
-m_proton = 938. *u.MeV
-#Emin for integration (Kelner eq 78)
-Emin_g = Egs + m_pion **2. / (4. * Egs) 
 
 #Define a convenient function
 def find_nearest(array, value):
@@ -52,7 +27,7 @@ class SNR_Cloud_Flux:
     from cosmic ray interactions in molecular clouds illuminated by nearby supernova remnants.
     """
     
-    def __init__(self, chi=0.05, distance_SNR=2000*u.pc, radius_MC=10*u.pc, accel_type='Impulsive', 
+    def __init__(self, chi=0.05, distance_SNR=2000*u.pc, radius_MC=10*u.pc, Eg_lo=1.0*u.GeV, Eg_hi=3e3*u.TeV, accel_type='Impulsive', 
                  snr_typeII=True, F_gal=False, palpha=2.0, D_fast=True, flag_low=True):
         """
         Initialize the SNR Cloud Flux Calculator by setting the properties of the SNR and the cloud.
@@ -67,6 +42,10 @@ class SNR_Cloud_Flux:
             Distance from earth to the SNR. (~ pc)
         radius_MC: astropy.Quantity
             Radius of the molecular cloud (~ pc)
+        Eg_lo: astropy.Quantity
+            Minimum energy of the produced gamma rays.
+        Eg_hi: astropy.Quantity
+            Maximum energy of the produced gamma rays.
         accel_type : str
             Type of acceleration. Possible values are:
             ``Impulsive`` or ``Continuous``
@@ -82,6 +61,34 @@ class SNR_Cloud_Flux:
         flag_low : boolean
             Flag to include low energy correction
         """
+        # Set up initial energy arrays
+        N = 200 
+        # Eg_lo = 10. *u.GeV
+        # Eg_lo = 1. * u.GeV
+        # Eg_hi = 3e3 *u.TeV # 1 PeV
+
+        # Set up gamma-ray evaluation energies
+        Eg_edges = np.logspace(start = np.log10(Eg_lo.to(u.TeV).value),
+                            stop  = np.log10(Eg_hi.to(u.TeV).value),
+                            num   = N+1) *u.TeV
+        self.Egs  = np.sqrt(Eg_edges[:-1] * Eg_edges[1:]) # log bin centers
+        self.dEgs = np.diff(Eg_edges)
+
+        # Define proton energies
+        # Ep_edges = np.logspace(0, 6.48, 1001) *u.GeV #1 PeV
+        Ep_edges = np.logspace(1, 6.48, 1001) *u.GeV #1 PeV
+        self.Eps = np.sqrt(Ep_edges[:-1] * Ep_edges[1:]) # log bin centers
+        self.dEps = np.diff(Ep_edges)
+
+        #Define low energy part of proton energies 
+        Epl_edges = np.logspace(0, 3.24, 1001) *u.GeV
+        self.Epls = np.sqrt(Epl_edges[:-1] * Epl_edges[1:]) 
+        self.dEpls = np.diff(Epl_edges)
+
+        self.m_pion = 135. * u.MeV
+        self.m_proton = 938. *u.MeV
+        #Emin for integration (Kelner eq 78)
+        self.Emin_g = self.Egs + self.m_pion **2. / (4. * self.Egs) 
 
         self.chi = chi
         self.distance_SNR = distance_SNR
@@ -132,7 +139,7 @@ class SNR_Cloud_Flux:
             (cloud_depth, dism, ismtime, Resc)
         """
         # Compute Escape time
-        tesc = acce.escape_time(Ep=Eps, typeIIb=self.snr_typeII)
+        tesc = acce.escape_time(Ep=self.Eps, typeIIb=self.snr_typeII)
         
         # Compute size of SNR at escape time
         Resc = acce.SNR_Radius(time=tesc).to(u.pc)
@@ -148,7 +155,7 @@ class SNR_Cloud_Flux:
         d2cloud = d2cloud.clip(min=0.)
         
         # Compute travel time from Resc to cloud
-        traveltime = d2cloud ** 2 / tran.Diffusion_Coefficient(Eps, acce.nh2ism, ism=1) / 4.
+        traveltime = d2cloud ** 2 / tran.Diffusion_Coefficient(self.Eps, acce.nh2ism, ism=1) / 4.
         
         ismtime = np.minimum(timediff, traveltime)
         ismtime = ismtime.clip(min=0)
@@ -158,7 +165,7 @@ class SNR_Cloud_Flux:
         # Compute time spent in cloud & cloud penetration depth
         cloudtime = timediff - ismtime
         cloudtime = cloudtime.clip(min=0)
-        cloud_depth = tran.R_diffusion(Eps, cloudtime, nh2, chi=self.chi, ism=0)
+        cloud_depth = tran.R_diffusion(self.Eps, cloudtime, nh2, chi=self.chi, ism=0)
         
         return cloud_depth, dism, ismtime, Resc
     
@@ -188,20 +195,20 @@ class SNR_Cloud_Flux:
         if self.accel_type == 'Impulsive':
             if np.isclose(sum(dism).value, 0):
                 pflux_tmp = injection.compute_pflux_impulsive_extended(
-                    Resc, self.N0, Eps, dism, time_diff, nh2, chi=self.chi, ism=1, alpha=part.alpha)
+                    Resc, self.N0, self.Eps, dism, time_diff, nh2, chi=self.chi, ism=1, alpha=part.alpha)
                 pfluxlow_tmp = injection.compute_pflux_impulsive_extended(
-                    Resc, self.N0, Epls, dism, time_diff, nh2, chi=self.chi, ism=1, alpha=part.alpha)
+                    Resc, self.N0, self.Epls, dism, time_diff, nh2, chi=self.chi, ism=1, alpha=part.alpha)
             else:
                 pflux_tmp = injection.compute_pflux_impulsive_extended(
-                    Resc, self.N0, Eps, dism, time_diff, acce.nh2ism, chi=self.chi, ism=1, alpha=part.alpha)
+                    Resc, self.N0, self.Eps, dism, time_diff, acce.nh2ism, chi=self.chi, ism=1, alpha=part.alpha)
                 pfluxlow_tmp = injection.compute_pflux_impulsive_extended(
-                    Resc, self.N0, Epls, dism, time_diff, acce.nh2ism, chi=self.chi, ism=1, alpha=part.alpha)
+                    Resc, self.N0, self.Epls, dism, time_diff, acce.nh2ism, chi=self.chi, ism=1, alpha=part.alpha)
         
         elif self.accel_type == 'Continuous':
             pflux_tmp = injection.compute_pflux_continuous_extended(
-                Resc, self.N0, Eps, dism, ismtime, acce.nh2ism, chi=self.chi, ism=1, alpha=part.alpha)
+                Resc, self.N0, self.Eps, dism, ismtime, acce.nh2ism, chi=self.chi, ism=1, alpha=part.alpha)
             pfluxlow_tmp = injection.compute_pflux_continuous_extended(
-                Resc, self.N0, Epls, dism, ismtime, acce.nh2ism, chi=self.chi, ism=1, alpha=part.alpha)
+                Resc, self.N0, self.Epls, dism, ismtime, acce.nh2ism, chi=self.chi, ism=1, alpha=part.alpha)
         
         else:
             raise ValueError(f"Bad acceleration type: {self.accel_type}, choose between Impulsive or Continuous")
@@ -224,8 +231,8 @@ class SNR_Cloud_Flux:
             Modified proton flux with galactic contribution
         """
         if self.F_gal is not False:
-            fgal_AMSO2 = injection.compute_fgal(Eps)
-            fgal_DAMPE = injection.compute_fgal_dampe(Eps)
+            fgal_AMSO2 = injection.compute_fgal(self.Eps)
+            fgal_DAMPE = injection.compute_fgal_dampe(self.Eps)
             
             if self.F_gal == "AMS-O2":
                 gal_ratio = np.nanmean(pflux_tmp / fgal_AMSO2)
@@ -264,19 +271,19 @@ class SNR_Cloud_Flux:
             Total gamma-ray flux (~ TeV^-1 cm^-2 s^-1)
         """
         # Compute kernel function for gamma-ray flux
-        F_gamma = fl.compute_gamma_kernel(Egs, Eps)
+        F_gamma = fl.compute_gamma_kernel(self.Egs, self.Eps)
         
         # Get cross section (cm2) for given proton energy
-        sig = part.sig_ppEK(Ep=Eps)
+        sig = part.sig_ppEK(Ep=self.Eps)
         
         # Compute gamma-ray flux
         intg = np.sum((sig[:,None] * pflux[:,None] * F_gamma) \
-                      * dEps[:,None] / Eps[:,None], axis=0)
+                      * self.dEps[:,None] / self.Eps[:,None], axis=0)
         
         phi = part.cspeed * nh2 * intg / (4. * np.pi * self.distance_SNR**2)
         phi = phi.to(1./(u.cm**2 * u.TeV * u.s))
         
-        total_phig = np.zeros(len(Egs)) * (1./(u.TeV*u.s*u.cm**2))
+        total_phig = np.zeros(len(self.Egs)) * (1./(u.TeV*u.s*u.cm**2))
         
         # Compute low energy part if requested
         if self.flag_low:
@@ -306,28 +313,28 @@ class SNR_Cloud_Flux:
         n_t = 1.  # Change to fix normalisation (1. as default)
         kappa = 1.
         K_pi = kappa / n_t
-        E_pi = (Epls - m_proton) * K_pi
-        dE_pi = dEpls
+        E_pi = (self.Epls - self.m_proton) * K_pi
+        dE_pi = self.dEpls
         
         # Compute q_pi
-        q_pi = (n_t / K_pi) * part.cspeed * part.sig_ppEK(Ep=Epls) * pfluxlow_tmp
+        q_pi = (n_t / K_pi) * part.cspeed * part.sig_ppEK(Ep=self.Epls) * pfluxlow_tmp
         
-        phig_low = np.zeros(len(Egs)) * (1./(u.TeV*u.s*u.cm**2))
+        phig_low = np.zeros(len(self.Egs)) * (1./(u.TeV*u.s*u.cm**2))
         
         # Integral eq 78 of Kelner from Emin for all Eg
-        for ig in range(len(Egs)):
-            mask = E_pi > Emin_g[ig]
-            phi_low = 2. * np.nansum(q_pi[mask] * dE_pi[mask] / np.sqrt(E_pi[mask]**2. - m_pion**2), axis=0)
+        for ig in range(len(self.Egs)):
+            mask = E_pi > self.Emin_g[ig]
+            phi_low = 2. * np.nansum(q_pi[mask] * dE_pi[mask] / np.sqrt(E_pi[mask]**2. - self.m_pion**2), axis=0)
             phi_low /= (4. * np.pi * self.distance_SNR**2)
             phig_low[ig] = phi_low
         
         # Establish normalisation factor
-        idxn = find_nearest(Egs, 0.1)  # at 200 GeV (smoother than 100 GeV)
+        idxn = find_nearest(self.Egs, 0.1)  # at 200 GeV (smoother than 100 GeV)
         phig_norm = phi[idxn] / phig_low[idxn]
         
-        Emask = Egs < Egs[idxn]
+        Emask = self.Egs < self.Egs[idxn]
         
-        total_phig = np.zeros(len(Egs)) * (1./(u.TeV*u.s*u.cm**2))
+        total_phig = np.zeros(len(self.Egs)) * (1./(u.TeV*u.s*u.cm**2))
         total_phig[~Emask] = phi[~Emask]
         total_phig[Emask] = phig_norm * phig_low[Emask]
         
@@ -351,23 +358,23 @@ class SNR_Cloud_Flux:
             [phi_nu, phi_nue_osc, phi_numu_osc, phi_nutau_osc] (~ TeV^-1 cm^-2 s^-1)
         """
         # Compute neutrino kernel function and neutrino flux
-        F_nu_1, F_nu_2 = fl.compute_neutrino_kernel(Egs, Eps)
+        F_nu_1, F_nu_2 = fl.compute_neutrino_kernel(self.Egs, self.Eps)
         
         # Get cross section
-        sig = part.sig_ppEK(Ep=Eps)
+        sig = part.sig_ppEK(Ep=self.Eps)
         
         # Total (for muon neutrinos, electron neutrinos are roughly F_nu_2 only)
         F_nu_mu = F_nu_1 + F_nu_2
         
         intn_mu = np.nansum((sig[:,None] * pflux[:,None] * F_nu_mu) \
-                          * dEps[:,None] / Eps[:,None], axis=0)
+                          * self.dEps[:,None] / self.Eps[:,None], axis=0)
         
         phi_numu = part.cspeed * nh2 * intn_mu / (4. * np.pi * self.distance_SNR**2)
         phi_numu = phi_numu.to(1./(u.cm**2 * u.TeV * u.s))
         
         F_nue = F_nu_2
         intn_e = np.nansum((sig[:,None] * pflux[:,None] * F_nue) \
-                          * dEps[:,None] / Eps[:,None], axis=0)
+                          * self.dEps[:,None] / self.Eps[:,None], axis=0)
         
         phi_nue = part.cspeed * nh2 * intn_e / (4. * np.pi * self.distance_SNR**2)
         phi_nue = phi_nue.to(1./(u.cm**2 * u.TeV * u.s))
@@ -436,7 +443,7 @@ class SNR_Cloud_Flux:
         # Compute neutrino flux
         phi_nu, phi_nue_osc, phi_numu_osc, phi_nutau_osc = self._compute_neutrino_flux(nh2, pflux)
         
-        return Egs, total_phig, phi_nu, phi_nue_osc, phi_numu_osc, phi_nutau_osc
+        return self.Egs, total_phig, phi_nu, phi_nue_osc, phi_numu_osc, phi_nutau_osc
     
     # def set_parameters(self, **kwargs):
     #     """
